66738394847bef3e667926a90813c53516fbdba5
fnspecter69 <nfreddy49@gmail.com>
2021-08-18 07:08:20 -0400

    update EmailController

--- src/main/java/com/example/controller/EmailController.java
+++ src/main/java/com/example/controller/EmailController.java
@@ -2,25 +2,42 @@ package com.example.controller;
 
 import com.example.api.Email;
 import com.example.api.EmailDao;
-import com.example.dao.MongoEmailDao;
 import java.util.List;
 
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailController {
-    private MongoTemplate mongoTemplate;
     private EmailDao emailDao;
 
-    public EmailController(MongoTemplate mongoTemplate){
-        emailDao = new MongoEmailDao(mongoTemplate);
+    public EmailController(EmailDao emailDao){
+        this.emailDao = emailDao;
     }
 
-    @RequestMapping(value = "/email/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/email/{id}")
+    @ResponseBody
     public Email getEmailById(@RequestParam String id) {
         return this.emailDao.getById(id);
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/email")
+    @ResponseBody
+    public Email createEmail(@RequestBody Email email) {
+        return this.emailDao.insertEmail(email);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/emails/{sender}")
+    @ResponseBody
+    public List<Email> getEmailsBySender(@RequestParam String sender) {
+        return emailDao.getBySender(sender);
+    }
+
+    public Email deleteEmail(@RequestParam String id) { return null; }
+
+    public Email updateEmail(Email email) { return null; }
 }
