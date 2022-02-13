f0f5e6cf5e31f447a57c8024bee1d548c5caa4f2
fnspecter69 <nfreddy49@gmail.com>
2022-02-13 15:23:45 -0500

    update unit test

--- src/test/java/com/example/validate/ValidateEmailTest.java
+++ src/test/java/com/example/validate/ValidateEmailTest.java
@@ -1,7 +1,7 @@
 package com.example.validate;
 
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
@@ -20,26 +20,25 @@ public class ValidateEmailTest {
     private String emailInvalid;
     private String emailInvalid2;
     private String emailInvalid3;
+    private ValidateEmail validateEmail;
     private ArrayList<String> validEmails = new ArrayList<>();
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        System.out.println("Beggining the tests\n\n\n\n");
         emailValid = "fre45@gamil.com";
         emailValid2 = "user-name@domain.com";
         emailValid3 = "username@domain.co.in";
+        emailInvalid = "yahoo.com";
+        emailInvalid2 = ".user.name@domain.com";
+        emailInvalid3 = "user-name@domain.com.";
         validEmails.add(emailValid);
         validEmails.add(emailValid2);
         validEmails.add(emailValid3);
+        validateEmail = new ValidateEmail();
     }
 
     @Test
     public void validateEmailAddressNormal() {
-        emailValid = "fre45@gamil.com";
-        emailValid2 = "user-name@domain.com";
-        emailValid3 = "username@domain.co.in";
-        System.out.println("\n\n\n\n\nvalidEmails size is " + validEmails.size());
-        ValidateEmail validateEmail = new ValidateEmail();
         Assert.assertTrue(validateEmail.isValidEmailAddress(emailValid));
         Assert.assertTrue(validateEmail.isValidEmailAddress(emailValid2));
         Assert.assertTrue(validateEmail.isValidEmailAddress(emailValid3));
@@ -47,12 +46,8 @@ public class ValidateEmailTest {
 
     @Test
     public void validateEmailAddressInvalid() {
-        emailInvalid = "yahoo.com";
-        emailInvalid2 = ".user.name@domain.com";
-        emailInvalid3 = "user-name@domain.com.";
-        ValidateEmail inValidateEmail = new ValidateEmail();
-        Assert.assertFalse(inValidateEmail.isValidEmailAddress(emailInvalid));
-        Assert.assertFalse(inValidateEmail.isValidEmailAddress(emailInvalid2));
-        Assert.assertFalse(inValidateEmail.isValidEmailAddress(emailInvalid3));
+        Assert.assertFalse(validateEmail.isValidEmailAddress(emailInvalid));
+        Assert.assertFalse(validateEmail.isValidEmailAddress(emailInvalid2));
+        Assert.assertFalse(validateEmail.isValidEmailAddress(emailInvalid3));
     }
 }
